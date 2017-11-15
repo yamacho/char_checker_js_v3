501c3759b8548a94708216262d394f580428cc1f
yamacho <yamacho@users.noreply.github.com>
2017-11-15 18:03:35 +0900

    JavaScript を書いていこう
    * script タグ
    * 変数の初期化
    * label の表示

---
+++ js/main.js
@@ -0,0 +1,9 @@
+(function() {
+  'use strict';
+
+  var comment = document.getElementById('comment');
+  var label = document.getElementById('label');
+  var LIMIT = 30;
+
+  label.innerHTML = LIMIT;
+})();
